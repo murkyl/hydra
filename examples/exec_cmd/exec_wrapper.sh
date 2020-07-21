14827bc9e7893bbb9e35cac3f6a695005078effe
Chung <Andrew.Chung@emc.com>
2020-07-21 13:27:03 +0800

    Fix wrapper script to run on all nodes for clients including the one that is running the server Add additional checks to prevent incorrect script execution with incorrect parameters Fix issue with interpreting some options for the command to run as options to the Python script Create 2 different screen sessions for server and client

--- examples/exec_cmd/exec_wrapper.sh
+++ examples/exec_cmd/exec_wrapper.sh
@@ -30,31 +30,51 @@ SERVER_OPTIONS="-v"
 
 # Standard options below. Should not need to modify these normally
 NUM_WORKERS=4
-SVR_NODE=`isi_nodes -L "%{lnn}"`
-SVR_IP=`isi_nodes -L "%{internal}"`
 SCRIPT="$BASE_PATH/exec_cmd.py"
 CLIENT_LOG=" -l ${LOG_PATH}/client_$(hostname).log"
 SERVER_LOG=" -l ${LOG_PATH}/server.log"
+SVR_IP=`isi_nodes -L "%{internal}"`
 
 
 function cleanup() {
   echo "Cleaning up old 'screen' sessions"
   isi_for_array -X \
+      screen -d -r exec_cmd_s -X quit &> /dev/null
+  isi_for_array -X \
       screen -d -r exec_cmd -X quit &> /dev/null
 }
 
-function check_dirs() {
+function check_params() {
+  err=0
+  screen_exe=`which screen`
+  if [ "${screen_exe}" == "" ]; then
+    echo "This script depends on the 'screen' binary being available. Install 'screen' and re-run."
+    err=1
+  fi
+  if [ ! -f "${SCRIPT}" ]; then
+    echo "Script to execute not found. Check the SCRIPT variable."
+    err=1
+  fi
   if [ ! -d "${BASE_PATH}" ]; then
     echo "Base path does not exist. Check script variables."
-    exit 2
+    err=1
   fi
   if [ ! -d "${LOG_PATH}" ]; then
     echo "Log path does not exist. Check script variables."
+    err=1
+  fi
+  if [ "${err}" == 1 ]; then
     exit 2
   fi
 }
 
-check_dirs
+function start_clients() {
+  isi_for_array -X \
+      screen -d -m -S exec_cmd \
+      bash "${BASE_PATH}/$0 client ${SVR_IP}"
+}
+
+check_params
 if [ "$1" == "client" ]; then
   python ${SCRIPT} -c $2 -n ${NUM_WORKERS} ${CLIENT_LOG} ${CLIENT_OPTIONS}
 elif [ "$1" == "cleanup" ]; then
@@ -62,14 +82,12 @@ elif [ "$1" == "cleanup" ]; then
   exit 1
 elif [ "$1" == "start" ]; then
   cleanup
-  echo "Starting server on current node"
+  echo "Starting server"
   shift
-  screen -d -m -S exec_cmd \
-      python ${SCRIPT} -s -p ${PROCESS_PATH} ${SERVER_LOG} ${SERVER_OPTIONS} $@
-  echo "Starting clients on all other nodes"
-  isi_for_array -x ${SVR_NODE} -X \
-      screen -d -m -S exec_cmd \
-      bash "${BASE_PATH}/$0 client ${SVR_IP}"
+  screen -d -m -S exec_cmd_s \
+      python ${SCRIPT} -s -p ${PROCESS_PATH} ${SERVER_LOG} ${SERVER_OPTIONS} -- $@
+  echo "Starting clients"
+  start_clients
 else
   cat <<- EOF
 Usage:
@@ -87,7 +105,7 @@ The root path is coded into the script itself.
 You can monitor the progress by 'tail'ing the server log file, connecting
 to the detatched screen session or by monitoring when the screen session
 terminates. When running 'screen -ls' to monitor progress, look for the
-session named <pid>.exec_cmd
+session named <pid>.exec_cmd_s
 EOF
   exit 1
 fi
